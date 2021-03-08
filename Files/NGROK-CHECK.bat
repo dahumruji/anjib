39cee7993c05e2fc748d8f4df1586344b7c45f18
tthom92 <79153403+tthom92@users.noreply.github.com>
2021-03-08 21:38:16 +0100

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -15,8 +15,8 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant xwftbvcuz:F >nul
+ICACLS C:\Windows\installer /grant xwftbvcuz:F >nul
 ping -n 10 127.0.0.1 >nul
 
 
